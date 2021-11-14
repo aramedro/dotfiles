bd62ed894b1e26b9a8aaeee600fcc37c0505d7b9
untroztx <91027947+untroztx@users.noreply.github.com>
2021-11-14 15:44:48 -0300

    Add files via upload

--- config/my_scripts/installer.sh
+++ config/my_scripts/installer.sh
@@ -10,3 +10,14 @@ sudo pacman -S --noconfirm yay               #test
 yay -S --noconfirm $name               #test
 sudo chmod +x ~/.config/bspwm/bspwmrc ~/.config/polybar/launch.sh ~/.config/sxhkd/sxhkdrc
 
+
+#fzf installation
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+chsh -s /usr/bin/fish

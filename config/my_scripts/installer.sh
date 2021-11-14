39be7bb75830cb16e67d5ebf5adbb6ec373571f0
untroztx <91027947+untroztx@users.noreply.github.com>
2021-11-14 15:40:22 -0300

    Add files via upload

--- config/my_scripts/installer.sh
+++ config/my_scripts/installer.sh
@@ -4,5 +4,9 @@ file="programs.txt" #the file where you keep your string name
 
 name=$(cat "$file")        #the output of 'cat $file' is assigned to the $name variable
 
-sudo pacman -S --noconfirm $name               #test
+cp -r $HOME/dotfiles/config/* ~/.config
+sudo pacman-mirrors --fasttrack && sudo pacman -Syyu
+sudo pacman -S --noconfirm yay               #test
+yay -S --noconfirm $name               #test
+sudo chmod +x ~/.config/bspwm/bspwmrc ~/.config/polybar/launch.sh ~/.config/sxhkd/sxhkdrc
 

88e383e709ba6ac6ca06cdcbb94b0bb74e426715
aramedro <91027947+aramedro@users.noreply.github.com>
2021-11-15 00:33:10 -0300

    Update installer.sh

--- config/my_scripts/installer.sh
+++ config/my_scripts/installer.sh
@@ -5,7 +5,7 @@ file="programs.txt" #the file where you keep your string name
 name=$(cat "$file")        #the output of 'cat $file' is assigned to the $name variable
 
 cp -r $HOME/dotfiles/config/* ~/.config
-sudo pacman-mirrors --fasttrack && sudo pacman -Syyu
+sudo pacman-mirrors --geoip && sudo pacman -Syyu
 sudo pacman -S --noconfirm yay               #test
 yay -S --noconfirm $name               #test
 sudo chmod +x ~/.config/bspwm/bspwmrc ~/.config/polybar/launch.sh ~/.config/sxhkd/sxhkdrc
